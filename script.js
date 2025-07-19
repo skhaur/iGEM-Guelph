a13ac8a393aa32dc0f519ad3c4c010e9be9347a3
Sukhman Kaur <sukhmankaur@Sukhmans-MacBook-Pro-8.local>
2025-07-19 17:25:55 -0400

    carosel applied, sectional texts added

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ burger.addEventListener('click', () => {
     burger.innerHTML = '&#9776;';
   }
 });
+
+const track = document.querySelector('.carousel-track');
+const leftBtn = document.querySelector('.carousel-btn.left');
+const rightBtn = document.querySelector('.carousel-btn.right');
+let currentIndex = 0;
+
+const updateCarousel = () => {
+  const slideWidth = document.querySelector('.carousel-img').clientWidth;
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+};
+
+rightBtn.addEventListener('click', () => {
+  const slides = document.querySelectorAll('.carousel-img');
+  if (currentIndex < slides.length - 1) {
+    currentIndex++;
+    updateCarousel();
+  }
+});
+
+leftBtn.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+});
