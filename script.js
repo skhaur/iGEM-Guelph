55de97cc1c37dc22a7adaaf107974f389b714420
Sukhman Kaur <sukhmankaur@Sukhmans-MacBook-Pro-8.local>
2025-07-19 16:31:00 -0400

    half way home page

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-console.log("iGEM site loaded!");
-document.querySelector(".burger").addEventListener("click", () => {
-    document.querySelector(".nav-links").classList.toggle("active");
-  });
-  
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  // Toggle between ☰ and ✕
+  if (navLinks.classList.contains('active')) {
+    burger.innerHTML = '&times;';
+  } else {
+    burger.innerHTML = '&#9776;';
+  }
+});
